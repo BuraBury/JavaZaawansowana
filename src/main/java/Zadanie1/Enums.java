15cf90efea2cada30539c65bc294bd40e4f50c5f
Paulina <bura.bury@gmail.com>
2020-06-27 11:57:51 +0200

    27.06 zadanie1

--- src/main/java/Zadanie1/Enums.java
+++ src/main/java/Zadanie1/Enums.java
@@ -1,11 +1,33 @@
 package Zadanie1;
 
 public enum Enums {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MONDAY(),
+    TUESDAY(),
+    WEDNESDAY(),
+    THURSDAY(),
+    FRIDAY(),
+    SATURDAY(),
+    SUNDAY();
+
+    public boolean isWeekDay() {
+        return this != SATURDAY && this != SUNDAY;
+    }
+
+    public boolean isHoliday() {
+        return this == SATURDAY || this == SUNDAY;
+    }
+
+    public String testDay() {
+        if (this == SATURDAY || this == SUNDAY) {
+            return "Today is time to Rest";
+        } else {
+            return "Go to work";
+        }
+    }
+
+
+
+
+
+
 }
--- src/main/java/Zadanie1/Main.java
+++ src/main/java/Zadanie1/Main.java
@@ -1,7 +1,23 @@
 package Zadanie1;
 
+import java.util.EnumSet;
+
 public class Main {
     public static void main(String[] args) {
+
+        Enums monday = Enums.MONDAY;
+        Enums tuesday = Enums.TUESDAY;
+        Enums wednesday = Enums.WEDNESDAY;
+        Enums thursday = Enums.THURSDAY;
+        Enums friday = Enums.FRIDAY;
+        Enums saturday = Enums.SATURDAY;
+        Enums sunday = Enums.SUNDAY;
+
+        Enums[] days = Enums.values();
+
+        for (Enums d : days) {
+            System.out.println(d.testDay());
+        }
 
     }
 }
