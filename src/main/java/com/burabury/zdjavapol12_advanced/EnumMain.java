5afca8d5166eee1355bc557a9840e2580629e84b
Paulina <bura.bury@gmail.com>
2020-06-27 11:26:10 +0200

    27.06

--- src/main/java/com/burabury/zdjavapol12_advanced/EnumMain.java
+++ src/main/java/com/burabury/zdjavapol12_advanced/EnumMain.java
@@ -1,5 +1,8 @@
 package com.burabury.zdjavapol12_advanced;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class EnumMain {
     public static void main(String[] args) {
 
@@ -17,17 +20,33 @@ public class EnumMain {
 
         for (Seasons s : seasons) {
             System.out.println(s);
+            System.out.println(s.getDaysCounter() + " days");
         }
 
         System.out.println();
 
-        int winterNumber = Seasons.WINTER.ordinal();
+        int winterNumber = Seasons.WINTER.ordinal(); //rzutowanie na numer
         System.out.println(winterNumber);
 
         System.out.println();
 
-        Seasons fromInt = Seasons.values()[winterNumber];
+        Seasons fromInt = Seasons.values()[1];
         System.out.println(fromInt);
+
+        System.out.println();
+
+        for (Seasons season : Seasons.values()) {
+            System.out.printf("%s %d%n", season.name(), season.ordinal());
+        }
+
+        System.out.println();
+
+        Set<Seasons> set = EnumSet.allOf(Seasons.class);
+        System.out.println(set);
+
+        System.out.println();
+
+        System.out.println(summer.getDaysCounter() + spring.getDaysCounter() + winter.getDaysCounter() + fall.getDaysCounter());
 
 
     }
--- src/main/java/com/burabury/zdjavapol12_advanced/Seasons.java
+++ src/main/java/com/burabury/zdjavapol12_advanced/Seasons.java
@@ -1,10 +1,21 @@
 package com.burabury.zdjavapol12_advanced;
 
 public enum Seasons {
-    SPRING,
-    SUMMER,
-    FALL,
-    WINTER
+    SPRING(90),
+    SUMMER(93),
+    FALL(89),
+    WINTER(88);
+
+    //dodawanie pol i metod
+    private int daysCounter;
+
+    Seasons(int daysCounter) {
+        this.daysCounter = daysCounter;
+    }
+
+    public int getDaysCounter() {
+        return daysCounter;
+    }
 
 
 }
