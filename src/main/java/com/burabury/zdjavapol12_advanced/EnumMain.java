94894c85aff8593ea0042b07b75d313fb660fd6c
Paulina <bura.bury@gmail.com>
2020-06-27 11:01:05 +0200

    27.06

--- src/main/java/com/burabury/zdjavapol12_advanced/EnumMain.java
+++ src/main/java/com/burabury/zdjavapol12_advanced/EnumMain.java
@@ -2,9 +2,35 @@ package com.burabury.zdjavapol12_advanced;
 
 public class EnumMain {
     public static void main(String[] args) {
-        Seasons winter = Seasons.WINTER;
+
         Seasons spring = Seasons.SPRING;
         Seasons summer = Seasons.SUMMER;
         Seasons fall = Seasons.FALL;
+        Seasons winter = Seasons.WINTER;
+
+        Seasons.valueOf("WINTER");
+        System.out.println(winter.toString());
+
+        System.out.println();
+
+        Seasons[] seasons = Seasons.values();
+
+        for (Seasons s : seasons) {
+            System.out.println(s);
+        }
+
+        System.out.println();
+
+        int winterNumber = Seasons.WINTER.ordinal();
+        System.out.println(winterNumber);
+
+        System.out.println();
+
+        Seasons fromInt = Seasons.values()[winterNumber];
+        System.out.println(fromInt);
+
+
     }
+
+
 }
