9316d3efdc227605f5dcae8c015db9bd63889b37
Paulina <bura.bury@gmail.com>
2020-06-27 10:35:54 +0200

    second commint

---
+++ src/main/java/com/burabury/zdjavapol12_advanced/EnumMain.java
@@ -0,0 +1,10 @@
+package com.burabury.zdjavapol12_advanced;
+
+public class EnumMain {
+    public static void main(String[] args) {
+        Seasons winter = Seasons.WINTER;
+        Seasons spring = Seasons.SPRING;
+        Seasons summer = Seasons.SUMMER;
+        Seasons fall = Seasons.FALL;
+    }
+}
---
+++ src/main/java/com/burabury/zdjavapol12_advanced/Seasons.java
@@ -0,0 +1,8 @@
+package com.burabury.zdjavapol12_advanced;
+
+public enum Seasons {
+    SPRING,
+    SUMMER,
+    FALL,
+    WINTER
+}
