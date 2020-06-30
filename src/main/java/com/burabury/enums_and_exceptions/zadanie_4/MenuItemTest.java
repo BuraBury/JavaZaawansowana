62bb2d40d67af38723ab9ed7746b6ab7f41b3e5a
Paulina <bura.bury@gmail.com>
2020-06-30 12:32:56 +0200

    Exercise 4/Enums

--- src/main/java/com/burabury/enums_and_exceptions/zadanie_4/MenuItemTest.java
+++ src/main/java/com/burabury/enums_and_exceptions/zadanie_4/MenuItemTest.java
@@ -1,11 +1,16 @@
 package com.burabury.enums_and_exceptions.zadanie_4;
 
-public class MenuItemTest{
+
+public class MenuItemTest {
     public static void main(String[] args) {
 
-
-
-
+        for (int i = 0; i < MenuItems.values().length; i++) {
+            System.out.println(MenuItems.values()[i] + ": ");
+            for (int j = 0; j < Language.values().length; j++) {
+                System.out.println(Language.values()[j] + ": " + MenuItems.values()[i].translate(Language.values()[j]));
+            }
+            System.out.println();
+        }
 
 
     }
