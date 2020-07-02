ba8310871a719f15a17fc57a379b9129b8acbf38
Paulina <bura.bury@gmail.com>
2020-07-02 17:32:17 +0200

    update

--- src/main/java/com/burabury/exceptions/imprezaAkademik/ImprezaAkademik.java
+++ src/main/java/com/burabury/exceptions/imprezaAkademik/ImprezaAkademik.java
@@ -1,26 +1,34 @@
 package com.burabury.exceptions.imprezaAkademik;
 
+import java.util.Random;
+
 public class ImprezaAkademik {
 
     public void party(int drunkLevel) throws HangoverException, UnexpectedSituationException {
 
-        if (drunkLevel < 5) {
+        Random random = new Random();
+        drunkLevel = random.nextInt(55);
+
+        if (drunkLevel > 0 && drunkLevel <= 5) {
             throw new UnexpectedSituationException();
-        } else if (drunkLevel <= 20) {
-            throw new HangoverException();
-        } else if (drunkLevel <= 30) {
-            throw new LoosedKeyException();
-        } else if (drunkLevel <= 40) {
-            throw new LoosedWalletException();
-        } else if (drunkLevel <= 50) {
-            throw new BurningBabilonException();
-        } else {
+        } else if (drunkLevel > 5 && drunkLevel <= 20) {
+            return;
+        }
+
+
+        if (drunkLevel > 50) {
             throw new MarsMissionException();
+        } else if (drunkLevel > 40) {
+            throw new BurningBabilonException();
+        } else if (drunkLevel > 30) {
+            throw new LoosedWalletException();
+        } else if (drunkLevel > 20) {
+            throw new LoosedKeyException();
+        } else if (drunkLevel > 10) {
+            throw new HangoverException();
         }
 
     }
-
-
 
 
 }
---
+++ src/test/java/com/burabury/exceptions/imprezaAkademik/ImprezaAkademikTestTest.java
@@ -0,0 +1,11 @@
+package com.burabury.exceptions.imprezaAkademik;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImprezaAkademikTestTest {
+
+
+
+}
