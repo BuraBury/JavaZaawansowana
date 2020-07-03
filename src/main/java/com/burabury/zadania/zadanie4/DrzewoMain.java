c6089e49cee138890727850266c56c6a5df59afb
Paulina <bura.bury@gmail.com>
2020-07-03 10:22:13 +0200

    update

--- src/main/java/com/burabury/zadania/zadanie4/DrzewoMain.java
+++ src/main/java/com/burabury/zadania/zadanie4/DrzewoMain.java
@@ -9,11 +9,11 @@ public class DrzewoMain {
         DrzewoLisciaste dab = new DrzewoLisciaste("dab", false, 7, "przekrojowy", 5);
         DrzewoOwocowe jablon = new DrzewoOwocowe("jablon", false, 4, "przekroj", 2, "jabłko");
 
-        Las las = new Las(modrzew, buk, dab, jablon);
+        Las las = new Las(modrzew, buk, dab, jablon, modrzew2);
 
         System.out.println(las.toString());
 
-        System.out.println(modrzew.equals(modrzew2));
-        System.out.println(dab.equals(jablon));
+        System.out.println("\nDoes larch equals another larch with different height?\n" + modrzew.equals(modrzew2));
+        System.out.println("\nDoes larch equals apple tree?\n"+dab.equals(jablon));
     }
 }
